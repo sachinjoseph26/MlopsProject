b729d2c05620a74373a7a8e760b1b51aa08a140d
sachin joseph <sachinjoseph054@gmail.com>
2024-08-18 21:12:45 -0400

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://sachinjoseph054:kOpRxNfjcc1GC74w@asdcluster.id1l6xq.mongodb.net/?retryWrites=true&w=majority&appName=ASDCluster"
+uri = ""
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))

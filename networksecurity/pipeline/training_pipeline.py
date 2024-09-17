a2ea5bfab94bac2f3e60512c1d5a280611a11481
sachinpjaj26 <sachinjoseph054@gmail.com>
2024-09-16 23:39:37 -0400

    latest update

---
+++ networksecurity/pipeline/training_pipeline.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logger.logger import logging
+
+from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.model_evaluation import ModelEvaluation
+from networksecurity.components.model_pusher import ModelPusher
+from networksecurity.components.model_trainer import ModelTraining
+
